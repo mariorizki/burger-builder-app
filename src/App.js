3587c8ce2d2133b72774ad512d7060692a64493a
Mario Rizki <mariorizki07@gmail.com>
2021-03-21 03:18:43 +0700

    add css module to layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Hello</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
